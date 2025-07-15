c4575843dc71f2bace7529c85ff7f39bdc848126
Sinka <szinkamiza@gmail.com>
2025-07-15 18:33:58 -0300

    porta 80 no start

--- v.2/start.sh
+++ v.2/start.sh
@@ -4,4 +4,4 @@
 echo "Iniciando o servidor Gunicorn..."
 # Inicia o servidor Flask usando Gunicorn, que é apropriado para produção
 # Ele vai procurar pela variável 'app' dentro do arquivo 'api_server.py' na pasta 'API'
-gunicorn --bind 0.0.0.0:8080 --workers 1 "API.api_server:app"
+gunicorn --bind 0.0.0.0:80 --workers 1 "API.api_server:app"
